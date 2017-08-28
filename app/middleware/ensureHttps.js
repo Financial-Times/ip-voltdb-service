74ca1f0d3f2a3041a997eb22203acea2dabd2d7b
snozza <andrewcsnead@gmail.com>
2017-08-28 11:28:16 +0100

    temp logs

--- app/middleware/ensureHttps.js
+++ app/middleware/ensureHttps.js
@@ -2,7 +2,9 @@ const generateError = require('../utils/generateError');
 
 module.exports = (req, res, next) => {
   const forwarded = req.get('x-forwarded-proto');
+  console.log(forwarded);
   if (forwarded && forwarded !== 'https') {
+    console.log(forwarded);
     next(generateError('Please use HTTPS when submitting data to this server', 403));
     return;
   }
