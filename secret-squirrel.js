544d04741aab8abe1d0ebf858fefca1e5057be6e
richardtuft <richard.tu@ft.com>
2018-05-03 15:03:33 +0100

    adding secret squirrel  🐿 v2.8.0

---
+++ secret-squirrel.js
@@ -0,0 +1,44 @@
+module.exports = {
+	files: {
+		allow: [
+			'.eslintrc'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'\\+U7Qle4vdLqDdPt9m09K6yZhkLDTWGoUiUzrQCn\\+', // package-lock.json:79
+			'\\+Ih9WOjoZ8YoHCoav2csqn4FOz4Orldsy2dmDwmQ', // package-lock.json:90
+			'eRzhrN1WSINYCDCbrz796z37LOe3m5tmW7RQf6oBntukAG1nmovJvhnwHHRMAfeoItc1m2Hk02WER2aQ', // package-lock.json:123
+			'/G1W/laGWjzH90t1vEa2xuxHD5KUrSzp9pUSfTm\\+', // package-lock.json:128
+			'vOk6uEMctu0vQrvuSqFdJyqj1Q0S5VTDL79qtjo\\+', // package-lock.json:213
+			'3MYsjlwGTi0tjQ9ANXZu4', // package-lock.json:577
+			't5Zpu0LstAn4PVg8rVLKF', // package-lock.json:821
+			'hYD5i0aPN5QwZisEbDStI', // package-lock.json:988
+			'u3XTuL3pf7XhPvzVOXRGd', // package-lock.json:1187
+			'dge4ujbXMJrnihYMcL4AoOweGnw9Tp3kQuqy1Kx5c1qKjqvMJZ6nVJPMWJtKCTN72ZogH3oeSO9g9rXQ', // package-lock.json:1247|2009
+			'04fXhMKdCPhjwbqAa6HXWaMxj8Q4hQDIh7IadJQw', // package-lock.json:1308|1308
+			'INKG75MwmbLOUKkDWc7xA', // package-lock.json:1416
+			'wcS5vuPglyXdsQa3XB4wH', // package-lock.json:1564
+			'r8RgzlXKmFOhZpHBztvye', // package-lock.json:1593
+			'M8GDrPGTJ27KqYFDpp6Uv', // package-lock.json:1649
+			'eb96eF6klf5mFl5zQVPzY', // package-lock.json:1889
+			'/abLGzEnPENCQwmHf7sck8Oyu4ob3LgBxWWxRoM\\+', // package-lock.json:1939
+			'PaQqUC9SRmAiSA9CCCYd4', // package-lock.json:1967
+			'/YFdLrom0lRx1BHfRYskFHfIMVkGdp8\\+dIZaxuU\\+', // package-lock.json:1972
+			'rEaBd8SUNAWgkvyPKXYMb', // package-lock.json:2111
+			'CbcG379L1e\\+mWBnLvHWWeLs8GyV/EMw862uLI3c\\+', // package-lock.json:2639
+			'xxHRdYnKtcECzVg7xOWhflvJMnqcFZjw', // package-lock.json:2649
+			'SG9rTX95BYNmau8rGFfCeaT6T5OW1C2A', // package-lock.json:2663
+			'sAqqVW3YtEVoFQ7J0blT8', // package-lock.json:2890
+			'/ad/2oNbKtW9Hgovl5O1FvFWKkKblNXoN/Oou6\\+9', // package-lock.json:3017
+			'yKuMkiO6NIiKpkopeyiFO', // package-lock.json:3203
+			'PBrfhx6pzWyUMbbqK9dKD', // package-lock.json:3229
+			'32BBeABfUi8V60SQ5yR6G', // package-lock.json:3235
+			'ZvfOL46v1s7tZeJkyDGzU', // package-lock.json:3787
+			'6XDMfA/BTt87skxqJlO0lAh3Dker5zThcAxG6mKz', // package-lock.json:3912
+			'sFtLPeOgc8Pl7kQVOWv0woD87KTXVHPIAE842FGT' // package-lock.json:4066|4066
+		]
+	}
+};
